1691ecf273c28fbcdb32a102b2e787c5d78ff9c2
AndreSilva1993 <andreemanuel.1993@gmail.com>
2020-07-30 11:18:37 +0100

    Initial implementation of the component's redesign

--- casper-notice.js
+++ casper-notice.js
@@ -1,23 +1,3 @@
-/*
-  - Copyright (c) 2017 Cloudware S.A. All rights reserved.
-  -
-  - This file is part of casper-notice.
-  -
-  - casper-notice is free software: you can redistribute it and/or modify
-  - it under the terms of the GNU Affero General Public License as published by
-  - the Free Software Foundation, either version 3 of the License, or
-  - (at your option) any later version.
-  -
-  - casper-notice  is distributed in the hope that it will be useful,
-  - but WITHOUT ANY WARRANTY; without even the implied warranty of
-  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  - GNU General Public License for more details.
-  -
-  - You should have received a copy of the GNU Affero General Public License
-  - along with casper-notice.  If not, see <http://www.gnu.org/licenses/>.
-  -
- */
-
 import '@cloudware-casper/casper-icons/casper-icon.js';
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 
@@ -25,100 +5,129 @@ class CasperNotice extends PolymerElement {
 
   static get properties () {
     return {
+      /**
+       * The casper-notice's title.
+       *
+       * @type {String}
+       */
       title: String,
+      /**
+       * The casper-notice's message.
+       *
+       * @type {String}
+       */
       message: String,
+      /**
+       * The casper-notice's type which will change the component's icon and color palette.
+       *
+       * @type {String}
+       */
       type: {
         type: String,
         value: 'info',
-        observer: '__typeChanged'
+        observer: '__typeChanged',
+        reflectToAttribute: true
       }
     };
   }
 
-  static get template() {
+  static get template () {
     return html`
       <style>
-
         :host {
-          white-space: normal;
-        }
-
-        .info {
-          background-color: #d9edf7;
-          border-color: #bce8f1;
-          color: #3a87ad;
-        }
-
-        .warning {
-          background-color: #fcf8e3;
-          border-color: #fbeed5;
-          color: #c09853;
-        }
-
-        .exclamation {
-          background-color: #f2dede;
-          border-color: rgb(238, 211, 215);
-          color: #ff5757;
-        }
-
-        .notice {
-          border: 1px solid;
-          border-radius: 4px;
-          font-size: 14px;
           display: flex;
-          flex-direction: row;
-          padding: 6px;
+          border-width: 2px;
+          border-style: solid;
+          border-radius: 5px;
+          /* Info color palette */
+          --info-color: #3a87ad;
+          --info-border-color: #bce8f1;
+          --info-background-color: #d9edf7;
+          /* Warning color palette */
+          --warning-color: #c09853;
+          --warning-border-color: #fbeed5;
+          --warning-background-color: #fcf8e3;
+          /* Exclamation color paletter */
+          --exclamation-color: #ff5757;
+          --exclamation-border-color: rgb(238, 211, 215);
+          --exclamation-background-color: #f2dede;
         }
 
-        .title {
-          font-weight: 700;
-          text-align: left;
+        :host([type="info"]) {
+          color: var(--info-color);
+          border-color: var(--info-border-color);
+          background-color: var(--info-background-color);
         }
 
-        .message {
-          margin-top: 6px;
-          text-align: left;
-          font-weight: normal;
+        :host([type="warning"]) {
+          color: var(--warning-color);
+          border-color: var(--warning-border-color);
+          background-color: var(--warning-background-color);
         }
 
-        p {
-          margin: 0px;
+        :host([type="exclamation"]) {
+          color: var(--exclamation-color);
+          border-color: var(--exclamation-border-color);
+          background-color: var(--exclamation-background-color);
         }
 
-        .column {
+        :host([type="info"]) .icon-container { border-right-color: var(--info-border-color); }
+        :host([type="warning"]) .icon-container { border-right-color: var(--warning-border-color); }
+        :host([type="exclamation"]) .icon-container { border-right-color: var(--exclamation-border-color); }
+
+        .icon-container {
           display: flex;
+          padding: 10px;
+          align-items: center;
+          border-right-width: 2px;
+          border-right-style: solid;
+        }
+
+        .icon-container casper-icon {
+          width: 30px;
+          height: 30px;
+        }
+
+        .content-container {
+          display: flex;
+          padding: 20px;
+          padding-left: 10px;
           flex-direction: column;
         }
 
-        casper-icon {
-          min-width: 32px;
-          min-height: 32px;
-          margin-right: 10px;
+        .content-container .title {
+          font-size: 15px;
+          font-weight: bold;
+          margin-bottom: 8px;
         }
 
+        .content-container .content {
+          font-size: 13px;
+          text-align: justify;
+        }
       </style>
 
-      <div class$="notice [[type]]">
-        <casper-icon></casper-icon>
-        <div class="column">
-          <p class="title">[[title]]</p>
-          <div class="message">
-            <slot>
-              <div class="message">[[message]]</div>
-            </slot>
-          </div>
+      <div class="icon-container">
+        <casper-icon icon="[[__getIcon(type)]]"></casper-icon>
+      </div>
+      <div class="content-container">
+        <span class="title">[[title]]</span>
+        <div class="content">
+          <slot>[[message]]</slot>
         </div>
       </div>
     `;
   }
 
-  __typeChanged (type) {
-    const casperIconElement = this.shadowRoot.querySelector('casper-icon');
-
-    switch (type) {
-      case 'info': casperIconElement.icon = 'fa-light:info-circle'; break;
-      case 'warning': casperIconElement.icon = 'fa-light:exclamation-triangle'; break;
-      case 'exclamation': casperIconElement.icon = 'fa-light:exclamation-circle'; break;
+  /**
+   * This method will return the icon that should be used depending on the component's current type.
+   */
+  __getIcon () {
+    switch (this.type) {
+      case 'info': return 'fa-light:info-circle';
+      case 'success': return 'fa-light:check-circle';
+      case 'warning': return 'fa-light:exclamation-triangle';
+      case 'exclamation': return 'fa-light:exclamation-circle';
     }
   }
 }
